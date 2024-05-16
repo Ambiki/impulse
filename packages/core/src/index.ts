a86817ecc1569ea981b0e4c5baab4735a36ee027
Abeid Ahmed <abeidahmed92@gmail.com>
2024-05-16 19:29:24 +0530

    feat: export observers (#26)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,5 @@ export { default as target } from './decorators/target';
 export { default as targets } from './decorators/targets';
 export { default as ImpulseElement } from './element';
 export { default as lazyImport } from './lazy_import';
+export { default as AttributeObserver } from './observers/attribute_observer';
+export { default as ElementObserver } from './observers/element_observer';
