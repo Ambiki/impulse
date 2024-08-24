131dda837b406c7f474fcc51cc342efc1a5905ae
Abeid Ahmed <abeidahmed92@gmail.com>
2024-08-24 14:20:06 +0530

    refactor: remove `getAttributeValues` function (#35)

--- packages/core/src/helpers/dom.ts
+++ packages/core/src/helpers/dom.ts
@@ -1,24 +1,4 @@
 /**
- * Returns the attribute's value as an array.
- * @example
- * <div data-action="foo bar"></div>
- *
- * const element = document.querySelector('div');
- * getAttributeValues(element, 'data-action');
- * //=> ['foo', 'bar']
- *
- *
- * <div data-action="foo bar"></div>
- *
- * const element = document.querySelector('div');
- * getAttributeValues(element, 'invalid');
- * //=> []
- */
-export function getAttributeValues(target: Element, attributeName: string): string[] {
-  return target.getAttribute(attributeName)?.trim().split(/\s+/) || [];
-}
-
-/**
  * Returns a promise that is fulfilled when the document's `readyState` is `complete`.
  */
 export function domReady() {
