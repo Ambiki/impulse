1b0d73497441aaf96b4e62641c7582fb984dd28a
abeidahmed <abeidahmed92@gmail.com>
2026-03-31 15:44:45 +0400

    feat: export `SetMap`
    closes #79

---
+++ packages/core/src/data_structures/index.ts
@@ -0,0 +1,1 @@
+export { default as SetMap } from './set_map';
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
+export * from './data_structures';
 export * from './decorators/property';
 export * from './decorators/register_element';
 export * from './decorators/target';
